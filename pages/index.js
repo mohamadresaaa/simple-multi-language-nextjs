7ef4385a82eaa93453c05d52e324d56d14d41349
mohamadreza mosalli <mohamadresaaa@gmail.com>
2022-08-28 19:41:56 +0430

    add locales

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
+import { useTranslation } from "next-i18next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+
 
 export default function Home() {
+  const {t} = useTranslation()
+
   return (
     <div className="text-red-500 text-3xl text-center">
-      hello Nextjs
+      {t("hello")} Nextjs
     </div>
   )
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"]))
+    }
+  }
 }
